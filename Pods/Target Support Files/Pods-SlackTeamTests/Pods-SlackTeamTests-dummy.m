3902483e7291df60ad552e96c1db5e698f27a1aa
Nicholas Long <longsview@gmail.com>
2018-02-10 20:48:51 -0800

    Added initial podfile spec

---
+++ Pods/Target Support Files/Pods-SlackTeamTests/Pods-SlackTeamTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SlackTeamTests : NSObject
+@end
+@implementation PodsDummy_Pods_SlackTeamTests
+@end
---
+++ SlackTeam/Services/WebService.swift
@@ -0,0 +1,9 @@
+//
+//  WebService.swift
+//  SlackTeam
+//
+//  Created by Nicholas Long on 2/10/18.
+//  Copyright © 2018 Nicholas Long. All rights reserved.
+//
+
+import Foundation
