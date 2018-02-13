c1b6a0131eb182c1035662b2f53fef4f3f9d9988
Nicholas Long <longsview@gmail.com>
2018-02-13 01:15:35 -0800

    Added app icon ... finishing touches

--- SlackTeam/Services/WebService.swift
+++ SlackTeam/Services/WebService.swift
@@ -16,6 +16,10 @@ class WebService {
     
     static let token = "xoxp-4698769766-4698769768-266771053075-66c3498cd17d3c736b94fdd14307ef20"
     static let baseUrl = "https://slack.com/api/"
+    // This endpoint given as part of this excersize was not working
+    // so I am using the one above based on the test endpoints
+    // at https://api.slack.com/web#basics
+    //static let baseUrl = "https://api.slack.com/methods/"
     fileprivate let manager = AFHTTPSessionManager()
     
     init() {
